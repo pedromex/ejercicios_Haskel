297683ba792dab3d0649cea60d4bb1fafc8161a8
pedromex <47623184+pedromex@users.noreply.github.com>
2019-04-09 10:50:33 -0500

    Update and rename Actividad_1.hs to Ejercicio_1_Pedro.hs

--- Actividad_1.hs
+++
@@ -1,6 +0,0 @@
---Actividad 1 Determina el resultado de un número x elevado a una potencia n (6 pts)
-
-potencia :: Integer -> Integer -> Integer
-potencia m 0 = 1
-potencia m n = m*(potencia m (n-1))
---Ejemplo: potencia 2 4
