33546c68132c83f38f72ce692e1e05a5198570e8
pedromex <47623184+pedromex@users.noreply.github.com>
2019-04-09 11:04:41 -0500

    Update and rename Actividad_11.hs to Ejercicio_11_Pedro.hs

---
+++ Ejercicio_11_Pedro.hs
@@ -0,0 +1,18 @@
+--11. Realiza una función en Haskell que permita cargar calcular la unión, intersección ydiferencia de dos conjuntos datos. Para esto puede hacer uso de la librería “Data.set”
+
+
+import qualified Data.Set as Set
+
+lista1 = ['a','u','d','r','e','t']
+lista2 = ['u','o','r','j','n','k']
+
+set1 = Set.fromList lista1
+set2 = Set.fromList lista2
+
+union = Set.union set1 set2
+intersec = Set.intersection set1 set2
+dif = Set.difference set1 set2
+
+--Para realizar las operaciones de los dos conjuntos 
+--hay que llamar a la funcion
+
