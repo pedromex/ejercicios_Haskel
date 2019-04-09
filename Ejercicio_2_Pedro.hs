0983d7cb4e2b9ff12de1c9b79f0df267aa3228f0
pedromex <47623184+pedromex@users.noreply.github.com>
2019-04-09 11:07:53 -0500

    Update Ejercicio_2_Pedro.hs

--- Ejercicio_2_Pedro.hs
+++ Ejercicio_2_Pedro.hs
@@ -1,4 +1,4 @@
--- 2.-Determina si un número n se encuentra en un rango determinado
+-- 2.-Determina si un número n se encuentra en un rango determinado.
 
 contiene :: Int->Bool
 contiene n = n `elem` [1,2..10]
