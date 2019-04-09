32d454363dfe787fdfee0faa181b976e282b39f6
pedromex <47623184+pedromex@users.noreply.github.com>
2019-04-09 11:10:45 -0500

    Update Ejercicio_9_Pedro.hs

--- Ejercicio_9_Pedro.hs
+++ Ejercicio_9_Pedro.hs
@@ -1,4 +1,4 @@
---9.- Realizar un función recursiva que retorne como salida el resultado de la suma 1 + 3 + 5 + 7 + 9 + N 
+--9.- Realizar un función recursiva que retorne como salida el resultado de la suma 1 + 3 + 5 + 7 + 9 + N.
 sumaImpares :: Integer -> Integer
 sumaImpares n = sum [x | x <- [1,5..n]]
 
