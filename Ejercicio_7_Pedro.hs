f46d2387296074ee7342ce4baa117097d840afbf
pedromex <47623184+pedromex@users.noreply.github.com>
2019-04-09 10:58:17 -0500

    Update and rename Actividad_7.hs to Ejercicio_7_Pedro.hs

--- Ejercicio_7_Pedro.hs
+++ Ejercicio_7_Pedro.hs
@@ -1,8 +1,7 @@
---7. Determina si dada una lista, ésta se encuentra ordenada. Se debe devolver verdadero o falso. (9 pts)
+--7.- Determina si dada una lista, ésta se encuentra ordenada. Se debe devolver verdadero o falso.
 
 lista_ordenada::Ord a=>[a]->Bool
 lista_ordenada [] = True
 lista_ordenada [_] = True
 lista_ordenada (x:y:xs) = (x<=y) && lista_ordenada (y:xs)
 
---Ejemplo lista_ordenada [1,2,3,4,5,4]
