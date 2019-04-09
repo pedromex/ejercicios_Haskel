d77090cea99db36664aa4aa1fa4496935ac222ee
pedromex <47623184+pedromex@users.noreply.github.com>
2019-04-09 11:11:12 -0500

    Update Ejercicio_10_Pedro.hs

--- Ejercicio_10_Pedro.hs
+++ Ejercicio_10_Pedro.hs
@@ -1,4 +1,4 @@
---10.- Realizar una función que reciba una lista y devuelva empleando recursividad otra lista de los elementos pares.
+--10.- Realizar una función que reciba una lista y devuelva empleando recursividad otra lista de los elementos pares
 
 filtraPares xs = [ x | x <- xs, odd x == False]
 
