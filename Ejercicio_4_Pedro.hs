7f0aeab2ec9ac9cb3797cfd20ea61e13ba340e61
pedromex <47623184+pedromex@users.noreply.github.com>
2019-04-09 10:54:50 -0500

    Update and rename Actividad_4.hs to Ejercicio_4_Pedro.hs

--- Ejercicio_4_Pedro.hs
+++ Ejercicio_4_Pedro.hs
@@ -1,6 +1,5 @@
---4. Determine el mayor de 4 enteros (6 pts)
+--4.- Determine el mayor de 4 enteros
 
 maxCuatro :: Int -> Int -> Int -> Int ->Int
 maxCuatro w x y z = max w (max x (max y z))
 
---Ejemplo: maxCuatro 2 4 6 7
