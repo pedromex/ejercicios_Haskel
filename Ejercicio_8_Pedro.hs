552a801ec7e0f73e28890fb1ceff8c0c9032ab5e
pedromex <47623184+pedromex@users.noreply.github.com>
2019-04-09 11:10:22 -0500

    Update Ejercicio_8_Pedro.hs

--- Ejercicio_8_Pedro.hs
+++ Ejercicio_8_Pedro.hs
@@ -1,4 +1,4 @@
---8.- Dadas dos listas, determine si son iguales. Devolver verdadeo o falso.
+--8.- Dadas dos listas, determine si son iguales. Devolver verdadeo ó falso.
 
 igualLista:: Eq a => [a]->[a]->Bool
 igualLista l1 l2 = l1 == l2
